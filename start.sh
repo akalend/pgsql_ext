efa8c28c2f035f8c4356eab16d85694c90fc7366
Borden Bruce <bruce99823@gmail.com>
2021-12-05 02:17:09 +0300

    start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 docker build -t pgsql .
-docker run --rm -d  -p 0.0.0.0:5432:5432 pgsql /sbin/entrypoint.sh
+docker run --rm -d --name  pgsql  -p 0.0.0.0:5432:5432 pgsql /sbin/entrypoint.sh
 
+docker run --rm -it  -p 0.0.0.0:5432:5432 pgsql /sbin/entrypoint.sh
 
 # docker kill pgsql
-# docker rm   pgsql
 # docker rmi  pgsql
