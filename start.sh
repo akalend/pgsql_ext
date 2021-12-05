bd44ff234f413dbb0c8beb62c92708afd7db8a48
Borden Bruce <bruce99823@gmail.com>
2021-12-05 20:59:39 +0300

    uncomment start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# docker build -t pgsql .
+docker build -t pgsql .
 docker run --rm -d --name  pgsql  -p 0.0.0.0:5432:5432 pgsql 
 sleep 3
 psql -h 172.17.0.2 -p 5432 -U postgres  -f test.sql test
 
 docker kill pgsql
-# docker rmi  pgsql
+docker rmi  pgsql
